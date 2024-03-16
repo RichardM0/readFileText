7e2695368e8ec361d6e54fc907832e0114f3a70b
RichardM0 <86446417+RichardM0@users.noreply.github.com>
2024-03-16 12:05:18 -0400

    Update ReadTextFile.java

--- ReadTextFile.java
+++ ReadTextFile.java
@@ -9,7 +9,7 @@ import java.util.*;
 
 import java.io.File;
 
-public class ReadTextFile
+public class readTextFile
 {
     
 
@@ -21,17 +21,16 @@ public class ReadTextFile
        ArrayList<String> words = new ArrayList<String>();
        HashMap<String, Integer> wordCount = new HashMap<>();
        try{
-           File myFile = new File("sampleText.txt");
+           File myFile = new File("macbeth.txt");
            Scanner myReader = new Scanner(myFile);
            while(myReader.hasNext()){
-               String item = myReader.next().replaceAll("[,!?./\\-;:{}'\"]","");
+               String item = myReader.next().replaceAll("[,!?./\\-;:{}'\"]","").toLowerCase();
                words.add(item);
-               System.out.println(item);
                if(!wordCount.containsKey(item)){
-                   wordCount.put(item.toLowerCase(), 1);
+                   wordCount.put(item, 1);
                }
                else{
-                   wordCount.put(item.toLowerCase(), wordCount.get(item) + 1);
+                   wordCount.put(item, wordCount.get(item) + 1);
                }
            }
            myReader.close();
@@ -49,19 +48,17 @@ public class ReadTextFile
        }
        Collections.sort(commonWordList);
        Collections.reverse(commonWordList);
-       System.out.println(commonWordList);
-       List<Integer> tenCommonIndex = new ArrayList(Arrays.asList(commonWordList));
+       List<Integer> tenCommonIndex = new ArrayList<>();
        tenCommonIndex = commonWordList.subList(0, 10);
        ArrayList<String> topTenWords = new ArrayList<>();
        for(Integer i : tenCommonIndex){
            for(Map.Entry<String, Integer> entry : wordCount.entrySet()){
-               System.out.println(topTenWords.contains(entry.getValue()));
-               if(entry.getValue() == i && !(topTenWords.contains(entry.getValue()))){
+               if(entry.getValue() == i && !(topTenWords.contains(entry.getKey()))){
                    topTenWords.add(entry.getKey());
                }
            }
        }
-       return topTenWords;
+       return topTenWords.subList(0,10);
    }
 
    
